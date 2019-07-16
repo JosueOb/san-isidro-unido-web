3127e6934a3e6a89be60ada7d622c76abb3cb690
JosueOb <josuericardocando@hotmail.com>
2019-07-16 15:12:42 -0500

    Modificación de la migración referente a users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('first_name',25);
+            $table->string('last_name',45); 
+            $table->string('avatar',50)->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->boolean('state'); 
+            $table->string('basic_service_image')->nullable(); 
+            $table->string('number_phone',10)->nullable(); 
             $table->rememberToken();
             $table->timestamps();
         });
