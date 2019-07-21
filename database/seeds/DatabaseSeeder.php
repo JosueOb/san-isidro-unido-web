740f6c31433d4b9e279c6de43b059b0bc541b3e0
JosueOb <josuericardocando@hotmail.com>
2019-07-21 15:54:02 -0500

    Agregando avatar al registro del administrador utilizando la API UI Avatars

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,9 +15,11 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         //Se inserta un usuario
+        $avatar =  $avatar = 'https://ui-avatars.com/api/?name='.substr(env('USER_FIRST_NAME'),0,1).'+'.substr(env('USER_LAST_NAME'),0,1).'&size=255';
         $admin = User::create([
             'first_name'=>env('USER_FIRST_NAME'),
             'last_name'=>env('USER_LAST_NAME'),
+            'avatar'=> $avatar,
             'email'=> env('USER_EMAIL'),
             'password'=> password_hash(env('USER_PASSWORD'), PASSWORD_DEFAULT),
             'state'=>true,
