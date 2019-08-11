bacfe46feeccc261ff5647f96342d39c1b793717
JosueOb <josuericardocando@hotmail.com>
2019-08-11 13:39:32 -0500

    Edición de la vista de verificación de correo electrónico

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,21 +1,29 @@
-@extends('layouts.app')
+@extends('layouts.authentication')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+            <div class="login-brand d-none d-md-block">
+                <img src="{{ asset('storage/img/logo.svg')}}" alt="logo" class="rounded-circle">
+            </div>
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h4>Verifica tu correo electrónico</h4>
+                </div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            <p>Se ha enviado un nuevo enlace de verificación a su correo electrónico</p>
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                    <p>
+                        Antes de continuar, revisa tu correo electrónico para ver si hay un enlace de verificación, 
+                        Si no recibes el correo <a href="{{ route('verification.resend') }}"> haga clic aquí para solicitar otro</a>
+                    </p>
+                
                 </div>
             </div>
         </div>
