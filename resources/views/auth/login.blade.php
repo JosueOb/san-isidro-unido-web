7c9e1d40809a1446ab30f68d6f07453cd421b6b2
JosueOb <josuericardocando@hotmail.com>
2019-08-12 14:10:24 -0500

    Cambios en la vista de restablecer password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.authentication')
+
 @section('content')
 <div class="container mt-5">
   <div class="row">
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,11 +1,16 @@
-@extends('layouts.app')
+@extends('layouts.authentication')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+            <div class="login-brand d-none d-md-block">
+                <img src="{{ asset('storage/img/logo.svg')}}" alt="logo" class="rounded-circle">
+            </div>
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h4>Restablecer contraseña</h4>
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -17,26 +22,23 @@
                     <form method="POST" action="{{ route('password.email') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <div class="form-group">
+                            <label for="email">Correco electrónico</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                            <p class="text-muted mt-2"> Ingresa tu correo electrónico para envió del enlace de restablecimiento de contraseña</p>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-sm btn-block">
+                                Enviar
+                            </button>
                         </div>
                     </form>
                 </div>
