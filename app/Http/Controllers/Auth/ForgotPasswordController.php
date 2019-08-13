146971c7ac2c7843cebbf05229170984cdbd2168
JosueOb <josuericardocando@hotmail.com>
2019-08-13 16:30:41 -0500

    Sobrescribiendo el método de envío del correo

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
+use App\User;
 
 class ForgotPasswordController extends Controller
 {
@@ -28,5 +30,30 @@ class ForgotPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    public function sendResetLinkEmail(Request $request){
+        $validData = $request->validate([
+            'email'=>'required|email|exists:users,email',
+        ],[
+            'email.required'=> 'El campo :attribute es obligatorio',
+            'email.email' => 'Ingrese un correo electrónico válido',
+            'email.exists' => 'No se encuentra ningún usuario registrado con el correo electrónico ingresado'
+        ]);
+        $user = User::where('email', $validData['email'])->first();
+        if($user->state && $user->getRol()){
+            // dd('Usuario activo con el rol de '.$user->getRol()->name);
+            try {
+                //code...
+                $this->broker()->sendResetLink(
+                    $this->credentials($request)
+                );
+                return back()->with('status', 'Enlace para restablecer su contraseña a sido enviado correctamente');
+            } catch (\Exception  $e) {
+                return abort(500);
+            }
+        }else{
+            return back()->with('status', 'Usuario no registrado en el sistema');
+        }
     }
 }
