59268f3d0f8089b4791d4894a3f60230b77cf8b1
JosueOb <josuericardocando@hotmail.com>
2020-01-21 17:52:01 -0500

    Craeación de la tmigración para la tabla Public Services

---
+++ database/migrations/2020_01_21_223641_create_public_services_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePublicServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('public_services', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 60);
+            $table->text('description');
+            $table->json('ubication')->nullable();
+            $table->unsignedBigInteger('category_id')->index();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('public_services');
+    }
+}
