63871df0896efed0aa32acff30f794cf952d1fd9
StalinMazaEPN <stalinct97@gmail.com>
2020-03-15 17:00:03 -0500

    agregado soporte a amazon s3

--- config/siu_config.php
+++ config/siu_config.php
@@ -15,5 +15,5 @@ return [
     ],
     "ONESIGNAL_APP_ID" => env('ONESIGNAL_APP_ID', ''),
     "ONESIGNAL_REST_API_KEY" => env('ONESIGNAL_REST_API_KEY', 'mysql'),
-    "API_IMAGES_DISK" => "images"
+    "API_IMAGES_DISK" => env('API_IMAGES_DISK', "images")
 ];
