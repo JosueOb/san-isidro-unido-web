68984d67c8d315da355a8e2ab585624d0ad1a341
JosueOb <josuericardocando@hotmail.com>
2020-06-07 10:50:35 -0500

    Estilos para la sección de noticias del landingPage

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -227,6 +227,156 @@
             </section>
 
 
+            <section id="news">
+                <div class="news-content">
+                    <h2 class="section-title">Noticias</h2>
+                    <div class="section-description">
+                        <p>Últimas actividades realizadas por parte de la directiva</p>
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
+                            <!-- <div id="news-carousel" class="carousel slide" data-ride="carousel"> -->
+                            <div id="news-carousel" class="carousel slide">
+                                <ol class="carousel-indicators">
+                                    <li data-target="#news-carousel" data-slide-to="0" class="active"></li>
+                                    <li data-target="#news-carousel" data-slide-to="1"></li>
+                                    <li data-target="#news-carousel" data-slide-to="2"></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <img src="https://images.pexels.com/photos/445109/pexels-photo-445109.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                                            alt="">
+                                        <div class="carousel-caption">
+                                            <h5>Fumigación de las calles principales del barrio</h5>
+
+                                            <p>Escrito por: Josué Cando</p>
+                                            <!-- Button trigger modal -->
+                                            <button type="button" class="btn btn-dark" data-toggle="modal"
+                                                data-target="#exampleModalScrollable">
+                                                Ver más información
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img class="d-block w-100"
+                                            src="https://images.pexels.com/photos/1839564/pexels-photo-1839564.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                                            alt="">
+                                        <div class="carousel-caption">
+                                            <h5>Title</h5>
+                                            <p>Text</p>
+                                        </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img class="d-block w-100"
+                                            src="https://images.pexels.com/photos/3922221/pexels-photo-3922221.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                                            alt="">
+                                        <div class="carousel-caption">
+                                            <h5>Title</h5>
+                                            <p>Text</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <a class="carousel-control-prev" href="#news-carousel" role="button" data-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next" href="#news-carousel" role="button" data-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </div>
+
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog"
+                                aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-scrollable" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalScrollableTitle">
+                                                Hola mundoHola mundoHola mundoHola mundoHola mundoHola mundoHola
+                                                mundoHola mundoHola mundoHola mundoHola mundoHola mundo</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+
+                                            <p class="text-right">Escrito por: Josué Cando</p>
+                                            <p class="text-justify">
+                                                Contrary to popular belief, Lorem Ipsum is not simply random text. It
+                                                has
+                                                roots in a piece of classical Latin literature from 45 BC, making it
+                                                over
+                                                2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
+                                                College in Virginia, looked up one of the more obscure Latin words,
+                                                consectetur, from a Lorem Ipsum passage, and going through the cites of
+                                                the
+                                                word in classical literature, discovered the undoubtable source. Lorem
+                                                Ipsum
+                                                comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et
+                                                Malorum"
+                                                (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book
+                                                is a
+                                                treatise on the theory of ethics, very popular during the Renaissance.
+                                                The
+                                                first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a
+                                                line
+                                                in section 1.10.32.
+
+                                                The standard chunk of Lorem Ipsum used since the 1500s is reproduced
+                                                below
+                                                for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus
+                                                Bonorum
+                                                et Malorum" by Cicero are also reproduced in their exact original form,
+                                                accompanied by English versions from the 1914 translation by H. Rackham.
+                                                Contrary to popular belief, Lorem Ipsum is not simply random text. It
+                                                has
+                                                roots in a piece of classical Latin literature from 45 BC, making it
+                                                over
+                                                2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
+                                                College in Virginia, looked up one of the more obscure Latin words,
+                                                consectetur, from a Lorem Ipsum passage, and going through the cites of
+                                                the
+                                                word in classical literature, discovered the undoubtable source. Lorem
+                                                Ipsum
+                                                comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et
+                                                Malorum"
+                                                (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book
+                                                is a
+                                                treatise on the theory of ethics, very popular during the Renaissance.
+                                                The
+                                                first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a
+                                                line
+                                                in section 1.10.32.
+
+                                                The standard chunk of Lorem Ipsum used since the 1500s is reproduced
+                                                below
+                                                for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus
+                                                Bonorum
+                                                et Malorum" by Cicero are also reproduced in their exact original form,
+                                                accompanied by English versions from the 1914 translation by H. Rackham.
+                                            </p>
+
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary"
+                                                data-dismiss="modal">Close</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            
         </div>
     </main>
     <!--/Main layout-->
