19bbf669b038c5b6c2f1810a1add254d007f09a1
JosueOb <josuericardocando@hotmail.com>
2020-06-13 23:32:36 -0500

    Registro de emergencias

--- app/HelpersClass/AdditionalData.php
+++ app/HelpersClass/AdditionalData.php
@@ -12,10 +12,19 @@ class AdditionalData
     public function __construct() 
     {
         $this->emergency = [
-            "attended_by" => null,
-            'rechazed_by' => null,
-            'rechazed_reason' => null,
-            "approved_by" => null, 
+            "approved" => [
+                'who'=>null,//usuario que aprobó el problema
+                'date'=>null,//fecha de aprobación
+            ],
+            "rechazed"=>[
+                'who'=>null,//usuario que rechazó el problema social
+                'reason'=>null,//razón del rechazo del problema social
+                'date'=>null,//fecha de rechado
+            ],
+            "attended"=>[
+                'who'=>null,//usuario que cambió a solucionado el problema
+                'date'=>null,//fecha en la que se atendió al problema
+            ],
             "status_attendance" => 'pendiente' //atendido, rechazado, pendiente
         ];
         $this->event = [
--- database/seeds/TestNotificationsSeeder.php
+++ database/seeds/TestNotificationsSeeder.php
@@ -44,7 +44,9 @@ class TestNotificationsSeeder extends Seeder
         $faker = Faker\Factory::create();
         // $additionalData = new AdditionalData();
 
-        $neighbors->each(function ($neighbor, $key) use ($problem_category, $problem_subcategory, $faker, $moderators_active) {
+        $emergency_category = Category::where('slug', 'emergencia')->first();
+
+        $neighbors->each(function ($neighbor, $key) use ($problem_category, $problem_subcategory, $emergency_category, $faker, $moderators_active) {
             $additionalData = new AdditionalData();
             $additional_data = $additionalData->getInfoSocialProblem();
             $ubication = [
@@ -53,7 +55,7 @@ class TestNotificationsSeeder extends Seeder
                 'address' => $faker->address,
                 'description' => $faker->text($maxNbChars = 30),
             ];
-
+            //Registro de problemas sociales reportados
             $problem = Post::create([
                 'title' => 'Problema' . ' ' . $key,
                 'description' => 'Descripción' . ' ' . $key,
@@ -67,6 +69,20 @@ class TestNotificationsSeeder extends Seeder
             ]);
 
             Notification::send($moderators_active, new SocialProblem($problem, $neighbor));
+
+            $additionalDataEmergency = new AdditionalData();
+            $additional_data_emergency = $additionalDataEmergency->getEmergencyData();
+            //Registro de emergencias reportadas
+            $emergency = Post::create([
+                'title' => 'Emergencia' . ' ' . $key,
+                'description' => 'Descripción' . ' ' . $key,
+                'category_id' => $emergency_category->id,
+                'state' => false,
+                'user_id' => $neighbor->id,
+                'ubication' => json_encode($ubication),
+                'additional_data' => $additional_data_emergency,
+            ]);
+            
         });
 
     }
